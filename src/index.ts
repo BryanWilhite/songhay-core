8d2efbb20de05fe634deb13a33db83890c729cd6
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-02-01 18:54:56 -0800

    :tractor: added index of exports

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/* models */
+export { CssLinearGradientData } from './models/songhay-css-linear-gradient-data';
+export { CssRedGreenBlue } from './models/songhay-css-red-green-blue';
+export { FunctionDisplayModel } from './models/songhay-function-display-model';
+
+/* utilities */
+export { CssUtility } from './utilities/songhay-css.utility';
+export { MathUtility } from './utilities/songhay-math.utility';
