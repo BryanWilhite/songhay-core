fe80b4541173cad4cc93ead075de2ba41c294a7c
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-02-22 15:20:25 -0800

    :snowflake: :rocket: issue #4

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 /* models */
-export { AssemblyInfo } from './src/models/assembly-info';
-export { CssLinearGradientData } from './src/models/css-linear-gradient-data';
-export { CssRedGreenBlue } from './src/models/css-red-green-blue';
-export { Document } from './src/models/document';
-export { Fragment } from './src/models/fragment';
-export { FunctionDisplayModel } from './src/models/function-display-model';
-export { Presentation } from './src/models/presentation';
-export { Segment } from './src/models/segment';
-export { SyndicationFeed } from './src/models/syndication-feed';
+export { AssemblyInfo } from './models/assembly-info';
+export { CssLinearGradientData } from './models/css-linear-gradient-data';
+export { CssRedGreenBlue } from './models/css-red-green-blue';
+export { Document } from './models/document';
+export { Fragment } from './models/fragment';
+export { FunctionDisplayModel } from './models/function-display-model';
+export { Presentation } from './models/presentation';
+export { Segment } from './models/segment';
+export { SyndicationFeed } from './models/syndication-feed';
 
 /* utilities */
-export { ArrayUtility } from './src/utilities/array.utility';
-export { CssUtility } from './src/utilities/css.utility';
-export { DomUtility } from './src/utilities/dom.utility';
-export { MapObjectUtility } from './src/utilities/map-object.utility';
-export { MathUtility } from './src/utilities/math.utility';
-export { ObjectUtility } from './src/utilities/object.utility';
+export { ArrayUtility } from './utilities/array.utility';
+export { CssUtility } from './utilities/css.utility';
+export { DomUtility } from './utilities/dom.utility';
+export { MapObjectUtility } from './utilities/map-object.utility';
+export { MathUtility } from './utilities/math.utility';
+export { ObjectUtility } from './utilities/object.utility';
