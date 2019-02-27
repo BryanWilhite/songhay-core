bc5b29fdf7f7aebfa4115e5757c734938c6734d6
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-02-27 15:06:16 -0800

    :umbrella: coverage for ArrayUtility

--- src/utilities/array.utility.spec.ts
+++ src/utilities/array.utility.spec.ts
@@ -1,5 +1,34 @@
 import { ArrayUtility } from './array.utility';
 
+test('should find duplicate numbers', () => {
+    const data = [1, 1, 2, 3, 4, 5.01, 55, 5, 7, 7, 7];
+    const duplicates = ArrayUtility.findDuplicates(data);
+    expect(duplicates).toBeDefined();
+    expect(duplicates).toBeTruthy();
+    expect(duplicates.length).toEqual(2);
+});
+
+test('should find duplicate text', () => {
+    const data = ['three', 'three', 'four', '3.0', '3', '3', 'one'];
+    const duplicates = ArrayUtility.findDuplicates(data);
+    expect(duplicates).toBeDefined();
+    expect(duplicates).toBeTruthy();
+    expect(duplicates.length).toEqual(2);
+});
+
+test('should get empty array', () => {
+    expect(ArrayUtility.isNotTruthyOrIsEmpty([])).toEqual(true);
+});
+
+test('should not get empty array', () => {
+    expect(ArrayUtility.isNotTruthyOrIsEmpty([1])).toEqual(false);
+});
+
+test('should get undefined array', () => {
+    const dummy: any = {};
+    expect(ArrayUtility.isNotTruthyOrIsEmpty(dummy.anything)).toEqual(true);
+});
+
 test('should sort numeric items', () => {
     let items = [{ item: 34 }, { item: 2 }, { item: 88 }, { item: -9 }];
     items = ArrayUtility.sortItems(items, 'item', true);
